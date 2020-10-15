d3908e211f7aa6533e74d3c17c09207454cf6265
Erick Luiz <si.erickluiz@gmail.com>
2020-10-14 23:35:38 -0300

    Update uri_1171_frequencia_de_numeros.java

--- modulo2/uri_1171_frequencia_de_numeros.java
+++ modulo2/uri_1171_frequencia_de_numeros.java
@@ -3,13 +3,10 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String... args){
-
+    public static void main(String... args) {
+    
         Scanner scanner = new Scanner(System.in);
         int nEntradas = scanner.nextInt();
-        if(nEntradas < 1 || nEntradas > 2000){
-            return;
-        }
         
         int[] valores = new int[nEntradas];
         for(int i = 0; i < nEntradas; i++){
@@ -19,7 +16,7 @@ public class Main {
         Arrays.sort(valores);
         for(int i = 0, count = 1; i < valores.length; i++){
             if(i == valores.length - 1 || valores[i] != valores[i + 1]){
-                System.out.printf("%d aparece %d vez(es)%n", valores[i], count);
+                System.out.println(valores[i] + " aparece "+ count+ " vez(es)");
                 count = 1;
                 continue;
             }
