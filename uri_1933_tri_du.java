c1b782a5d0f9f1aca3842301aea8e0de9d6dd125
Erick Luiz <si.erickluiz@gmail.com>
2020-10-14 22:24:47 -0300

    Update uri_1933_tri_du.java
    Nova versão do desafio

--- uri_1933_tri_du.java
+++ uri_1933_tri_du.java
@@ -1,48 +1,13 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String args[]) throws IOException {
+    public static void main(String args[]) {
 
-		var leitorDeTela = new LeitorDeTela();
-        int[] valores = leitorDeTela.leSequenciaNumerica();
-        if(valores[0] > valores[1]) {
-            System.out.println(valores[0]);
-        }else{
-            System.out.println(valores[1]);
-        }
-	}
-}
-
-// Proteger Leitor
-class LeitorDeTela {
-
-	private BufferedReader in;
-
-	public LeitorDeTela() {
-		InputStreamReader ir = new InputStreamReader(System.in);
-		this.in = new BufferedReader(ir);
-	}
-
-	public String leProximaLinha() throws IOException {
-		return this.in.readLine();
-	}
-	
-	public int leProximoNumero() throws IOException {
-		return Integer.valueOf(this.leProximaLinha());
-	}
-	
-	public int[] leSequenciaNumerica() throws IOException {
-	    // Proteger Leitor
-	    String entrada = this.leProximaLinha();
-        String valores[] = entrada.split(" ");
-		int[] resultadoDaLeitura = new int[valores.length];
-		
-		for(int i = 0; i < valores.length; i++){
-		  resultadoDaLeitura[i] = Integer.valueOf(valores[i]);
-		}
-		
-		return resultadoDaLeitura;
-	}
+	Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        
+        int c = a > b? a: b;
+        System.out.println(c);
+    }
 }
