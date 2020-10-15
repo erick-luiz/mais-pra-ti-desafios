829c94733c86fb2e723451d96db0b088c0ce3c72
Erick Luiz <si.erickluiz@gmail.com>
2020-10-14 22:05:26 -0300

    Update uri_1793_escada_rolante.java
    Nova versão do desafio

--- uri_1793_escada_rolante.java
+++ uri_1793_escada_rolante.java
@@ -1,68 +1,31 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String args[]) throws IOException {
-
-		var leitorDeTela = new LeitorDeTela();
-        List<Integer> tempos = new ArrayList<>();
+    
+    public static void main(String args[]) {
         
-        var numeroDePessoas = 0;
-        while((numeroDePessoas = leitorDeTela.leProximoNumero()) != 0) {
-            int[] valores = leitorDeTela.leSequenciaNumerica();
+        Scanner scanner = new Scanner(System.in);
+        
+        int numeroDePessoas = scanner.nextInt();
+        
+        while(numeroDePessoas != 0) {
+            
             int resultado = 10;
-            for(int i = 1; i < valores.length; i++){
-                var diferenca = valores[i] - valores[i-1];
-                if(diferenca > 10){
-                    resultado += 10; 
-                }else{
-                    resultado += diferenca;
+            int valorAnterior = scanner.nextInt();
+            
+            for(int i = 1; i < numeroDePessoas; i++){
+                int valorAtual = scanner.nextInt();
+                if(valorAtual - valorAnterior > 10){
+                    resultado += 10;
+                }else {
+                    resultado += valorAtual - valorAnterior;
                 }
+                valorAnterior = valorAtual;
             }
-            tempos.add(resultado);
-            // impedir infinito
+
+            System.out.println(resultado);
+            numeroDePessoas = scanner.nextInt();
         }
-        
-        
-        for(Integer tempo: tempos){
-            System.out.println(tempo);
-        }
-	}
-}
-
-// Proteger Leitor
-class LeitorDeTela {
-
-	private BufferedReader in;
-
-	public LeitorDeTela() {
-		InputStreamReader ir = new InputStreamReader(System.in);
-		this.in = new BufferedReader(ir);
-	}
-
-	public String leProximaLinha() throws IOException {
-		return this.in.readLine();
-	}
-	
-	public int leProximoNumero() throws IOException {
-		return Integer.valueOf(this.leProximaLinha());
-	}
-	
-	public int[] leSequenciaNumerica() throws IOException {
-	    // Proteger Leitor
-	    String entrada = this.leProximaLinha();
-        String valores[] = entrada.split(" ");
-		int[] resultadoDaLeitura = new int[valores.length];
-		
-		for(int i = 0; i < valores.length; i++){
-		  resultadoDaLeitura[i] = Integer.valueOf(valores[i]);
-		}
-		
-		return resultadoDaLeitura;
-	}
-	
-	
+    }
 }
